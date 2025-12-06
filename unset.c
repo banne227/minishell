e94ca11d56513cce0747423906483b84ab2dce32
banne <baidyanne9@gmail.com>
2025-12-06 21:12:28 +0000

    42

---
+++ unset.c
@@ -0,0 +1,47 @@
+#include stdbool.h
+
+bool is_valid_arg(char *arg)
+{
+    int i;
+
+    i = 1;
+    if (!arg || (!ft_isalpha(arg[0]) && arg[0] != '_'))
+        return (false);
+    while (arg[i])
+    {
+        if (!ft_isalnum(arg[i]) && arg[i] != '_')
+            return (false);
+        i++;
+    } 
+    return (true);
+}
+
+bool env_exist(t_env *env, char *arg)
+{
+    int i;
+    int len;
+
+    i = 0;
+    len = strlen(arg);
+    while (env->envp[i])
+    {
+        if (ft_strncmp(env->envp[i], arg, len) == 0 && env->envp[i][len] == '=')
+            return (true);
+        i++;
+    }
+    return (false);
+}
+
+void ft_unset(t_env *env, char **args)
+{
+    int i;
+
+    i = 1;
+    while (args[i])
+    {
+        if (is_valid_arg(args[i]) && env_exist(env, args[i]))
+            env_remove(env, args[i]);
+        i++;
+    }
+}
+
